99ccc278bae50ccd5406428ad136c90e75288d8f
Raul Guajardo <hitomisparda@gmail.com>
2018-06-30 00:39:26 -0400

    Updated Cider

--- init.el
+++ init.el
@@ -10,7 +10,10 @@
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
 ;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
